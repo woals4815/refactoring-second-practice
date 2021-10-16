a2f4ab75786114d9a69a8e4a905b843309366cf3
Jayden JaeMin Jeong <71315300+woals4815@users.noreply.github.com>
2021-10-16 17:44:53 +0900

    [refactoring] for asia variable

--- ch4/sample.test.js
+++ ch4/sample.test.js
@@ -1,11 +1,13 @@
 const { Province, sampleProvinceData } = require('./sample');
 describe('province', () => {
+  let asia;
+  beforeEach(() => {
+    asia = new Province(sampleProvinceData());
+  });
   it('shortfall', () => {
-    const asia = new Province(sampleProvinceData());
     expect(asia.shortfall).toBe(5);
   });
   it('profit', () => {
-    const asia = new Province(sampleProvinceData());
     expect(asia.profit).toBe(230);
   });
 });
