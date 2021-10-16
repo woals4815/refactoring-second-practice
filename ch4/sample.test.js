6be44ea484ed17699c5f92e6c4c6ebc3c466195a
Jayden JaeMin Jeong <71315300+woals4815@users.noreply.github.com>
2021-10-16 18:02:41 +0900

    add some edge cases

--- ch4/sample.test.js
+++ ch4/sample.test.js
@@ -15,4 +15,51 @@ describe('province', () => {
     expect(asia.shortfall).toBe(-6);
     expect(asia.profit).toBe(292);
   });
+  it('zero demand', () => {
+    asia.demand = 0;
+    expect(asia.shortfall).toBe(-25);
+    expect(asia.profit).toBe(0);
+  });
+  it('negative demand', () => {
+    asia.demand = -1;
+    expect(asia.shortfall).toBe(-26);
+    expect(asia.profit).toBe(-10);
+  });
+  it('empty string demand', () => {
+    asia.demand = '';
+    expect(asia.shortfall).toBe(NaN);
+    expect(asia.profit).toBe(NaN);
+  });
+});
+
+describe('no producers', () => {
+  let noProducers;
+  beforeEach(() => {
+    const data = {
+      name: 'no producers',
+      producers: [],
+      demand: 30,
+      price: 20,
+    };
+    noProducers = new Province(data);
+  });
+  it('shortfall', () => {
+    expect(noProducers.shortfall).toBe(30);
+  });
+  it('profit', () => {
+    expect(noProducers.profit).toBe(0);
+  });
+});
+
+describe('string for producers', () => {
+  it('', () => {
+    const data = {
+      name: 'string producers',
+      producers: '',
+      demand: 30,
+      price: 20,
+    };
+    const prov = new Province(data);
+    expect(prov.shortfall).toBe(0);
+  });
 });
