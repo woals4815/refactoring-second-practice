4c6b0af982ed97f7cf28b03dadfd1ccbf261ab15
Jayden JaeMin Jeong <71315300+woals4815@users.noreply.github.com>
2021-10-17 14:35:21 +0900

    smaple 5 step2

--- ch6/practice5.js
+++ ch6/practice5.js
@@ -3,6 +3,9 @@ class Book {
     this._reservations = [];
   }
   addReservation(customer) {
+    this.zz_addReservation(customer);
+  }
+  zz_addReservation(customer) {
     this._reservations.push(customer);
   }
 }
