a282ae4332a35b852be3c374e2fb9d3bce6ce34e
Jayden JaeMin Jeong <71315300+woals4815@users.noreply.github.com>
2021-10-17 13:21:55 +0900

    add ch6. original sample code

---
+++ ch6/practice.js
@@ -0,0 +1,22 @@
+function printOwing(invoice) {
+  let outstanding = 0;
+
+  console.log('**************');
+  console.log('고객채무');
+  console.log('**************');
+
+  for (const o of invoice.orders) {
+    outstanding += o.amount;
+  }
+
+  const today = new Date();
+  invoice.dueDate = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate() + 30
+  );
+
+  console.log(`customer: ${invoice.customer}`);
+  console.log(`amount: ${outstanding}`);
+  console.log(`Due date: ${invoice.dueDate.toLocaleDateString()}`);
+}
