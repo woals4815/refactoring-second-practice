6d0eb33fe10696a0c5fc147d80cc9419f03e863d
Jayden JaeMin Jeong <71315300+woals4815@users.noreply.github.com>
2021-10-17 14:05:11 +0900

    refactor abstraction step2

--- ch6/practice3.js
+++ ch6/practice3.js
@@ -1,8 +1,10 @@
 function price(order) {
   const basePrice = order.quantity * order.itemPrice;
+  const quantityDiscount =
+    Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
   return (
     basePrice -
-    Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
+    quantityDiscount +
     Math.min(order.quantity * order.itemPrice * 0.1, 100)
   );
 }
