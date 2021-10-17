ddbc1c4f837756a6e2ad7a77b2f69fcfdf574a40
Jayden JaeMin Jeong <71315300+woals4815@users.noreply.github.com>
2021-10-17 15:55:33 +0900

    add sammple 9 step1

---
+++ ch6/sample9.js
@@ -0,0 +1,26 @@
+const reading = { customer: 'john doe', quantity: 10, month: 5, year: 2017 };
+
+function aquireReading() {
+  return reading;
+}
+
+class Reading {
+  constructor(data) {
+    this._customer = data.customer;
+    this._quantity = data.quantity;
+    this._month = data.month;
+    this._year = data.year;
+  }
+  get customer() {
+    return this._customer;
+  }
+  get quantity() {
+    return this._quantity;
+  }
+  get month() {
+    return this._month;
+  }
+  get year() {
+    return this._year;
+  }
+}
