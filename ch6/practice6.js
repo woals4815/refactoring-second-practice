5bde2abdc967a8f3d4822d2c649a66db6be028cc
Jayden JaeMin Jeong <71315300+woals4815@users.noreply.github.com>
2021-10-17 15:07:10 +0900

    add sample6 step2

--- ch6/practice6.js
+++ ch6/practice6.js
@@ -3,3 +3,11 @@ let spaceship = { name: 'victory' };
 let defaultOwner = { firsName: 'martin', lastname: 'pauler' };
 
 spaceship.owner = defaultOwner;
+
+function getDefaultOnwer() {
+  return defaultOwner;
+}
+
+function setDefaultOnwer(arg) {
+  defaultOwner = arg;
+}
