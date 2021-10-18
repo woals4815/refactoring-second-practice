cf7e191513bbb808b048e6f9a941cf36c7f4381c
Jayden JaeMin Jeong <71315300+woals4815@users.noreply.github.com>
2021-10-18 20:36:02 +0900

    [add] sample2 step2

--- ch7/sample2.js
+++ ch7/sample2.js
@@ -13,6 +13,19 @@ class Person {
   set courses(aList) {
     this._courses = aList;
   }
+  addCourse(aCourse) {
+    this._courses.push(aCourse);
+  }
+  removeCourse(
+    aCourse,
+    fnIfAbsent = () => {
+      throw new RangeError();
+    }
+  ) {
+    const index = this._courses.indexOf(aCourse);
+    if (index === -1) fnIfAbsent();
+    else this._courses.splice(index, -1);
+  }
 }
 
 class Course {
