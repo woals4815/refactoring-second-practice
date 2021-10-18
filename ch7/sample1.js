57eb4f9885bd5f07e5cc32a670f7d63ddf336e9d
Jayden JaeMin Jeong <71315300+woals4815@users.noreply.github.com>
2021-10-18 20:06:44 +0900

    sample1 step1-1

--- ch7/sample1.js
+++ ch7/sample1.js
@@ -1,15 +1,47 @@
 class Org {
   constructor(data) {
-    this._data = data;
+    this._name = data.name;
+    this._country = data.country;
+  }
+  get name() {
+    return this._name;
+  }
+
+  set name(aString) {
+    return (this._name = aString);
+  }
+  get country() {
+    return this._country;
+  }
+  set country(aCountryCode) {
+    this._country = aCountryCode;
   }
 }
 
 const org = new Org({ name: 'jayden', country: 'KR' });
 
-function getRawDataOfOrg() {
-  return org._data;
-}
-
 function getOrg() {
   return org;
 }
+
+/* <--------------------------------> */
+
+class CustomerData {
+  constructor(data) {
+    this._data = data;
+  }
+}
+
+function getCustomerData() {
+  return customerData;
+}
+
+function getRawDataOfCustomers() {
+  return customerData._data;
+}
+
+function setRawDataOfCustomers(arg) {
+  customerData = new CustomerData(arg);
+}
+
+/* <--------------------------------> */
