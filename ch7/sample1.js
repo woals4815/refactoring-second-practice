86ece9a196821f041e205e57a40109ac17a65a79
Jayden JaeMin Jeong <71315300+woals4815@users.noreply.github.com>
2021-10-18 19:50:41 +0900

    sample1 step3

--- ch7/sample1.js
+++ ch7/sample1.js
@@ -1,5 +1,15 @@
-const org = { name: 'jayden jaemin jeong', country: 'KR' };
+class Org {
+  constructor(data) {
+    this._data = data;
+  }
+}
+
+const org = new Org({ name: 'jayden', country: 'KR' });
 
 function getRawDataOfOrg() {
+  return org._data;
+}
+
+function getOrg() {
   return org;
 }
