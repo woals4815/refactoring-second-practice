80486fc37b46edba6df749dc29d50e8d7e262d93
Jayden JaeMin Jeong <71315300+woals4815@users.noreply.github.com>
2021-10-19 18:06:12 +0900

    sample4.js step4,5,6

--- ch7/sample4.js
+++ ch7/sample4.js
@@ -4,12 +4,14 @@ class Order {
     this._item = item;
   }
   get price() {
-    var discountFactor = 0.98;
-
-    if (this.basePrice > 1000) discountFactor -= 0.3;
-    return this.basePrice * discountFactor;
+    return this.basePrice * this.discountFactor;
   }
   get basePrice() {
     return this._quantity * this._item.price;
+  }
+  get discountFactor() {
+    var discountFactor = 0.98;
+    if (this.basePrice > 1000) discountFactor -= 0.03;
+    return discountFactor;
   }
 }
