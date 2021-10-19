c2ec991b6c23ba050079db3ed485087e7cd0856f
Jayden JaeMin Jeong <71315300+woals4815@users.noreply.github.com>
2021-10-19 18:31:10 +0900

    Add sample5 step4

--- ch7/sample5.js
+++ ch7/sample5.js
@@ -12,10 +12,10 @@ class Person {
     return `(${this.officeAreaCode}) ${this.officeNumber}`;
   }
   get officeAreaCode() {
-    return this._officeAreaCode;
+    return this._telephoneNumber.officeAreaCode;
   }
   set officeAreaCode(arg) {
-    this._officeAreaCode = arg;
+    this._telephoneNumber.officeAreaCode = arg;
   }
   get officeNumber() {
     return this._officeNumber;
@@ -31,5 +31,12 @@ class TelephoneNumber {
   }
   set officeAreaCode(arg) {
     this._officeAreaCode = arg;
+  }
+
+  get officeNumber() {
+    return this._officeNumber;
+  }
+  set officeNumber(arg) {
+    this._officeNumber = arg;
   }
 }
